7c9db851b0b256abed3f018d18f50cc0cd536f35
Jakub Kubacki <kubacki.jk@gmail.com>
2014-08-12 10:31:33 +0200

    Disable before_save redbox migration.

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Product.class_eval do
   delegate_belongs_to :master, :redbox_product_id
-  before_save :migrate_redbox
+  # before_save :migrate_redbox
 
   def migrate_redbox
     Redbox::Migrate::RedboxProduct.new.migrate_product(self) if Rails.env.production?
