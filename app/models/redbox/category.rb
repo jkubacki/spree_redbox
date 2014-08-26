d2e4ac738408c1150f5021028722a9432c12ea87
Jakub Kubacki <kubacki.jk@gmail.com>
2014-08-26 11:56:48 +0200

    Unserialize name in category.

--- app/models/redbox/category.rb
+++ app/models/redbox/category.rb
@@ -1,4 +1,18 @@
 class Redbox::Category < ActiveRecord::Base
   establish_connection 'redbox'
   self.table_name = 'shop_category'
+  after_initialize :decode_strings
+  before_save :encode_strings
+
+
+  private
+  def decode_strings
+    decoder = Redbox::StringDecoder.new
+    self.name = decoder.decode(self.name)
+  end
+
+  def encode_strings
+    decoder = Redbox::StringDecoder.new
+    self.name = decoder.encode(self.name)
+  end
 end
