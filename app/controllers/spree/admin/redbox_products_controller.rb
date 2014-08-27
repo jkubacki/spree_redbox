c323485d70e2d4d187ec7870d4c7852ab92c1a9d
Jakub Kubacki <kubacki.jk@gmail.com>
2014-08-27 15:04:53 +0200

    Make controller save multiple products.

--- app/controllers/spree/admin/redbox_products_controller.rb
+++ app/controllers/spree/admin/redbox_products_controller.rb
@@ -7,13 +7,23 @@ module Spree
       end
 
       def index
-        @products = Redbox::Product.with_hash.limit(5).includes(:colors).select(:product_id, :name_storage, :image, :symbol, :only_courier, :light_package)
+        # @products = Redbox::Product.with_hash.limit(5).includes(:colors).select(:id, :name_storage, :image, :symbol, :only_courier, :light_package)
+        @products = Redbox::Product.with_hash.limit(5).select(:product_id, :name_storage, :image, :symbol, :only_courier, :light_package)
       end
 
       def update_multiple
-        # Redbox::Product.update(params[:products].keys, params[:products].values)
-        puts params.inspect
+        products = Redbox::Product.find(person_params.keys)
+        products.each.with_index do |product|
+          person_params[product.id.to_s].each { |key, value| eval "product.#{key} = #{value}" }
+          product.save
+        end
         redirect_to admin_redbox_products_path
+      end
+
+      private
+
+      def person_params
+        params[:products].permit!
       end
 
     end
