50d92e276590498414c34799a1c8e0e80bc7dae5
Jakub Kubacki <kubacki.jk@gmail.com>
2014-08-28 14:16:47 +0200

    Category tree exploring.

--- app/models/redbox/category.rb
+++ app/models/redbox/category.rb
@@ -4,6 +4,44 @@ class Redbox::Category < ActiveRecord::Base
   after_initialize :decode_strings
   before_save :encode_strings
 
+  scope :roots, -> { where("category_id LIKE '___'") }
+
+  def self.category_tree(start_node = new, categories = [])
+    start_node.children.each do |child|
+      tree = category_tree(child)
+      unless tree.empty?
+        categories << { child => category_tree(child) }
+      else
+        categories << child.id
+      end
+    end
+    categories
+  end
+
+  def is_root?
+    if categories.size == 1
+      true
+    else
+      false
+    end
+  end
+
+  def parent
+    return nil if is_root?
+    if Redbox::Category.exists? parent_number
+      Redbox::Category.find parent_number
+    else
+      nil
+    end
+  end
+
+  def children(cascade = false)
+    if cascade
+      Redbox::Category.where("category_id LIKE '#{id}_%'")
+    else
+      Redbox::Category.where("category_id LIKE '#{id}___'")
+    end
+  end
 
   private
   def decode_strings
@@ -14,5 +52,14 @@ class Redbox::Category < ActiveRecord::Base
   def encode_strings
     decoder = Redbox::StringDecoder.new
     self.name = decoder.encode(self.name)
+  end
+
+  def categories
+    id.to_s.scan /.{1,5}/
+  end
+
+  def parent_number
+    return nil if is_root?
+    id[0..-4]
   end
 end
