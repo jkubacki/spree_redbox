28a14a61718ff7afaf1edbb0d56853009aaf7e58
Jakub Kubacki <kubacki.jk@gmail.com>
2014-09-01 09:16:10 +0200

    Fix redbox_product controller

--- app/controllers/spree/admin/redbox_products_controller.rb
+++ app/controllers/spree/admin/redbox_products_controller.rb
@@ -4,21 +4,28 @@ module Spree
 
       def index
         get_data
-        category_id = form_params[:id]
-        @products = Redbox::Product.within_category(category_id).with_hash.includes(:colors, :ocassions, :styles, :genders, :sizes).select(:product_id, :name_storage, :image, :symbol, :only_courier, :light_package)
+        @category_id = form_params[:id]
+        @products = Redbox::Product.within_category(@category_id).with_hash.page(params[:page]).per(10).includes(:colors, :ocassions, :styles, :genders, :sizes).select(:product_id, :name_storage, :image, :symbol, :only_courier, :light_package)
+        @page = params[:page] || 1
       end
 
       def update_multiple
         Redbox::Product.find(products_params.keys).each.with_index do |product|
           product.update_attributes products_params[product.id.to_s]
         end
-        redirect_to admin_redbox_products_path
+        category_id = update_multiple_params[:category][:id]
+        page = update_multiple_params[:page].to_i + 1
+        redirect_to admin_redbox_products_path(category: {id: category_id}, page: page)
       end
 
       private
 
       def products_params
         params[:products].permit!
+      end
+
+      def update_multiple_params
+        params[:update_multiple]
       end
 
       def form_params
@@ -35,9 +42,13 @@ module Spree
         @styles = Redbox::Style.order('name ASC')
         @genders = Redbox::Gender.order('name ASC')
         @sizes = Redbox::Size.order('name ASC')
-        @categories = Redbox::Category.all_names Redbox::Category.find('046') # przebrania
+        @categories = Rails.cache.fetch(:get_categories) { get_categories }
       end
 
+      private
+      def get_categories
+        Redbox::Category.all_names(Redbox::Category.find('046'))
+      end
     end
   end
 end
